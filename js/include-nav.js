ce1cb928fc1476f9d25b23355a1fa89caee354dd
Catherine Bain <catherinebain@catherinebain.csun.edu>
2026-09-08 09:54:25 -0700

    fix the nav bar to be dynamic underline

--- js/include-nav.js
+++ js/include-nav.js
@@ -2,4 +2,25 @@ fetch('/nav.html')
   .then(res => res.text())
   .then(html => {
     document.getElementById('nav-placeholder').innerHTML = html;
+    highlightCurrentNavLink();
   });
+
+function highlightCurrentNavLink() {
+  // Current page filename; treat "/" and "" as index.html
+  var page = window.location.pathname.split('/').pop();
+  if (!page) page = 'index.html';
+
+  var links = document.querySelectorAll('.nav-links a');
+  for (var i = 0; i < links.length; i++) {
+    var link = links[i];
+    if (link.classList.contains('ext')) continue;
+    var target = link.getAttribute('href').split('/').pop();
+    if (target === page) {
+      link.classList.add('active');
+      link.setAttribute('aria-current', 'page');
+    } else {
+      link.classList.remove('active');
+      link.removeAttribute('aria-current');
+    }
+  }
+}
